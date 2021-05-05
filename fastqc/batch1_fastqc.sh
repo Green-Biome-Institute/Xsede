231b278c580f22d881b9ff362bbf645e31a34153
Melis Akman <makman@Meliss-MacBook-Pro.local>
2021-05-05 15:21:09 -0700

    fastqc

--- fastqc/batch1_fastqc.sh
+++ fastqc/batch1_fastqc.sh
@@ -1,10 +1,8 @@
 #!/bin/bash 
 #SBATCH -D /ocean/projects/bio210016p/shared/novaseq/batch1
 #SBATCH --account=bio210016p
-#SBATCH --partition=RM
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=4
-#SBATCH --cpus-per-task=4
+#SBATCH --partition=RM-shared
+#SBATCH --ntasks-per-node=24
 #SBATCH --time=10:00:00
 #SBATCH -o /ocean/projects/bio210016p/makman/outs/batch1_fastqc.out
 #SBATCH -e /ocean/projects/bio210016p/makman/outs/batch1_fastqc.err
@@ -18,10 +16,16 @@ module load FastQC
 # /opt/packages/FastQC/0.11.9/fastqc GBI_Apallida_S1_L001_R1_001.fastq.gz
 # /opt/packages/FastQC/0.11.9/fastqc GBI_Apallida_S1_L001_R2_001.fastq.gz
 # /opt/packages/FastQC/0.11.9/fastqc GBI_Ecovalifolium_S2_L001_R1_001.fastq.gz
-/opt/packages/FastQC/0.11.9/fastqc GBI_Ecovalifolium_S2_L001_R2_001.fastq.gz
-/opt/packages/FastQC/0.11.9/fastqc GBI_Fstriata_S4_L001_R1_001.fastq.gz
-/opt/packages/FastQC/0.11.9/fastqc GBI_Fstriata_S4_L001_R2_001.fastq.gz
-/opt/packages/FastQC/0.11.9/fastqc GBI_Mabbottii_S5_L001_R1_001.fastq.gz
-/opt/packages/FastQC/0.11.9/fastqc GBI_Mabbottii_S5_L001_R2_001.fastq.gz
+# /opt/packages/FastQC/0.11.9/fastqc GBI_Ecovalifolium_S2_L001_R2_001.fastq.gz
+# /opt/packages/FastQC/0.11.9/fastqc GBI_Fstriata_S4_L001_R1_001.fastq.gz
+# /opt/packages/FastQC/0.11.9/fastqc GBI_Fstriata_S4_L001_R2_001.fastq.gz
+# /opt/packages/FastQC/0.11.9/fastqc GBI_Mabbottii_S5_L001_R1_001.fastq.gz
+# /opt/packages/FastQC/0.11.9/fastqc GBI_Mabbottii_S5_L001_R2_001.fastq.gz
 
-# this used 285 SUs
+/opt/packages/FastQC/0.11.9/fastqc GBI_Adensiflora_S9_L001_R1_001.fastq.gz
+/opt/packages/FastQC/0.11.9/fastqc GBI_Adensiflora_S9_L001_R2_001.fastq.gz
+
+/opt/packages/FastQC/0.11.9/fastqc GBI_Fstriata_S10_L001_R1_001.fastq.gz
+/opt/packages/FastQC/0.11.9/fastqc GBI_Fstriata_S10_L001_R2_001.fastq.gz
+
+
