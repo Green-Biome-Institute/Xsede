a8788b879c591a05ed030455b8b841f93d874321
Melis Akman <makman@br011.ib.bridges2.psc.edu>
2021-05-14 15:25:33 -0400

    gzip

--- fastqc/batch1_blast.sh
+++ fastqc/batch1_blast.sh
@@ -25,7 +25,7 @@ module load BLAST/2.9.0
 # update_blastdb.pl --decompress nt [*] #39 SUs with 24 ntasks-per-node
 
 cd /ocean/projects/bio210016p/shared/novaseq/batch1/
-gzip *001.fastq.gz
+gzip *001.fastq
 # blastn -task blastn -num_descriptions 1 -num_alignments 1 -num_threads 24 -query GBI_Adensiflora_S3_L001_R1_001_100K.fastq -db /ocean/projects/bio210016p/shared/blast_databases/nt -out GBI_Adensiflora_S3_L001_R1_001_100K_blast.out
 # blastn -task blastn -num_descriptions 1 -num_alignments 1 -num_threads 48 -query GBI_Apallida_S1_L001_R1_001_100K.fastq -db /ocean/projects/bio210016p/shared/blast_databases/nt -out GBI_Apallida_S1_L001_R1_001_100K_blast.out
 # blastn -task blastn -num_descriptions 1 -num_alignments 1 -num_threads 72 -query GBI_Ecovalifolium_S2_L001_R1_001_100K.fastq -db /ocean/projects/bio210016p/shared/blast_databases/nt -out GBI_Ecovalifolium_S2_L001_R1_001_100K_blast.out
