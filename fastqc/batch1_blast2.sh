4091cb0cf6e26eb344294fb3d8abd053aa455e53
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2021-05-17 11:46:15 -0700

    blast

--- fastqc/batch1_blast2.sh
+++ fastqc/batch1_blast2.sh
@@ -2,8 +2,8 @@
 #SBATCH -D /ocean/projects/bio210016p/shared/novaseq/batch1
 #SBATCH --account=bio210016p
 #SBATCH --partition=RM-shared
-#SBATCH --ntasks-per-node=128
-#SBATCH --time=24:00:00
+#SBATCH --ntasks-per-node=64
+#SBATCH --time=48:00:00
 #SBATCH -o /ocean/projects/bio210016p/makman/outs/batch1_blast2.out
 #SBATCH -e /ocean/projects/bio210016p/makman/outs/batch1_blast2.err
 #SBATCH --mail-user=melis.akman@csueastbay.edu
@@ -18,7 +18,7 @@ module load BLAST/2.9.0
 
 sed -n '1~4s/^@/>/p;2~4p' GBI_Apallida_S1_L001_R1_001_50K.fastq > GBI_Apallida_S1_L001_R1_001_50K.fasta
 
-blastn -task blastn -num_descriptions 1 -num_alignments 1 -num_threads 128 -query GBI_Apallida_S1_L001_R1_001_50K.fasta -db /ocean/projects/bio210016p/shared/blast_databases/nt -out GBI_Apallida_S1_L001_R1_001_50K_blast.out
+blastn -task blastn -num_descriptions 1 -num_alignments 1 -num_threads 64 -query GBI_Apallida_S1_L001_R1_001_50K.fasta -db /ocean/projects/bio210016p/shared/blast_databases/nt -out GBI_Apallida_S1_L001_R1_001_50K_blast.out
 
 
 #failed timed out 2304 SUs
