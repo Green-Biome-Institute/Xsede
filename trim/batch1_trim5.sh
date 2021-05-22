aa098aa5ccd55ec51562e2d19889a452a190503b
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2021-05-21 20:59:19 -0700

    trim

--- trim/batch1_trim5.sh
+++ trim/batch1_trim5.sh
@@ -1,8 +1,8 @@
 #!/bin/bash 
 #SBATCH -D /ocean/projects/bio210016p/shared/novaseq/batch1/rawdata/
 #SBATCH --account=bio210016p
-#SBATCH --partition=RM-shared
-#SBATCH --ntasks-per-node=72
+#SBATCH --partition=RM
+#SBATCH --ntasks-per-node=64
 #SBATCH --time=48:00:00
 #SBATCH -o /ocean/projects/bio210016p/makman/outs/batch1_trim5.out
 #SBATCH -e /ocean/projects/bio210016p/makman/outs/batch1_trim5.err
@@ -23,4 +23,4 @@ module load python/3.8.6
 
 
 
-/ocean/projects/bio210016p/shared/programs/TrimGalore-0.6.6/trim_galore --paired --illumina --fastqc --gzip --cores 72 GBI_Mabbottii_S5_L001_R1_001.fastq.gz GBI_Mabbottii_S5_L001_R2_001.fastq.gz
+/ocean/projects/bio210016p/shared/programs/TrimGalore-0.6.6/trim_galore --paired --illumina --fastqc --gzip --cores 64 GBI_Mabbottii_S5_L001_R1_001.fastq.gz GBI_Mabbottii_S5_L001_R2_001.fastq.gz
