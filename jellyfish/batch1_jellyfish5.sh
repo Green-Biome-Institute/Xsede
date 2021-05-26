70d6d737ad0b7cfe258f60d0ae3ca7976bef63d2
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2021-05-26 13:40:12 -0700

    jellyfish histo

--- jellyfish/batch1_jellyfish5.sh
+++ jellyfish/batch1_jellyfish5.sh
@@ -14,3 +14,7 @@
 /ocean/projects/bio210016p/shared/programs/jellyfish-2.3.0/bin/jellyfish count -t 6 -C -m 20 -s 2G -o Mabbottii_20mer_out <(zcat ./GBI_Mabbottii_S5_L001_R1_001_val_1.fq.gz)
 /ocean/projects/bio210016p/shared/programs/jellyfish-2.3.0/bin/jellyfish count -t 6 -C -m 17 -s 2G -o Mabbottii_17mer_out <(zcat ./GBI_Mabbottii_S5_L001_R1_001_val_1.fq.gz)
 /ocean/projects/bio210016p/shared/programs/jellyfish-2.3.0/bin/jellyfish count -t 6 -C -m 23 -s 2G -o Mabbottii_23mer_out <(zcat ./GBI_Mabbottii_S5_L001_R1_001_val_1.fq.gz)
+
+jellyfish histo -o Mabbottii_20mer_out.histo Mabbottii_20mer_out
+jellyfish histo -o Mabbottii_17mer_out.histo Mabbottii_17mer_out
+jellyfish histo -o Mabbottii_23mer_out.histo Mabbottii_23mer_out
