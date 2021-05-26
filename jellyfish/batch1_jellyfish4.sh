8318958e4a41188ab04f14b9ce8b6d94d8e0c3f8
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2021-05-26 14:28:14 -0700

    jellyfish histo

--- jellyfish/batch1_jellyfish4.sh
+++ jellyfish/batch1_jellyfish4.sh
@@ -12,8 +12,8 @@
 
 # genome size ~ 90G, s=G + G*(error=0.01)*coverage (30X)= 120 round up to 120G
 
-/ocean/projects/bio210016p/shared/programs/jellyfish-2.3.0/bin/jellyfish count -t 128 -C -m 20 -s 80G -o Fstriata_20mer_out <(zcat ./GBI_Fstriata_S4_S10_L001_R1_001_val_1.fq.gz)
-/ocean/projects/bio210016p/shared/programs/jellyfish-2.3.0/bin/jellyfish count -t 128 -C -m 17 -s 80G -o Fstriata_17mer_out <(zcat ./GBI_Fstriata_S4_S10_L001_R1_001_val_1.fq.gz)
-/ocean/projects/bio210016p/shared/programs/jellyfish-2.3.0/bin/jellyfish count -t 128 -C -m 23 -s 80G -o Fstriata_23mer_out <(zcat ./GBI_Fstriata_S4_S10_L001_R1_001_val_1.fq.gz)
+/ocean/projects/bio210016p/shared/programs/jellyfish-2.3.0/bin/jellyfish count -t 128 -C -m 20 -s 60G -o Fstriata_20mer_out <(zcat ./GBI_Fstriata_S4_S10_L001_R1_001_val_1.fq.gz)
+/ocean/projects/bio210016p/shared/programs/jellyfish-2.3.0/bin/jellyfish count -t 128 -C -m 17 -s 60G -o Fstriata_17mer_out <(zcat ./GBI_Fstriata_S4_S10_L001_R1_001_val_1.fq.gz)
+/ocean/projects/bio210016p/shared/programs/jellyfish-2.3.0/bin/jellyfish count -t 128 -C -m 23 -s 60G -o Fstriata_23mer_out <(zcat ./GBI_Fstriata_S4_S10_L001_R1_001_val_1.fq.gz)
 
-#128 CPUs were not enough for s 120G.
+#128 CPUs were not enough for s 120G or 80G.
